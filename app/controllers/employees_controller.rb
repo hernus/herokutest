233d2edc23a79c86f47c9694e9ceeab20bf46c3d
Hernus <hernus@carelsen.com>
2012-07-22 12:53:33 +1000

    fix

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -2,7 +2,7 @@ class EmployeesController < ApplicationController
   # GET /employees
   # GET /employees.json
   def index
-#    @employees = Employee.all
+    @employees = Employee.all
 
     respond_to do |format|
       format.html # index.html.erb
