f099516d019a91f4d9432a965123a33528413d99
Hernus <hernus@carelsen.com>
2012-07-22 11:46:47 +1000

    roote change

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Herokutest::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'menus#menu1'
+   root :to => 'employees#index'
 
   # See how all your routes lay out with "rake routes"
 
